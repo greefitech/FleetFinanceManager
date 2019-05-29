20178f4503bb9815bb8f432f6db0d50c7d3b9426
spmohansp <spmohansp@gmail.com>
2019-05-29 14:29:33 +0530

    helper changes updated

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -2,10 +2,14 @@
 
 use Carbon\Carbon;
 
-function DateDifference($Date){
-    if(!empty($Date)){
-        return Carbon::now()->diffInDays($Date, false);
-    }else{
-        return '-';
+if (! function_exists('DateDifference')) {
+    function DateDifference($Date)
+    {
+        if (!empty($Date)) {
+            return Carbon::now()->diffInDays($Date, false);
+        } else {
+            return '-';
+        }
+
     }
 }
