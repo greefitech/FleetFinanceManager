4d8a14c248e3ab5372c37b4109ef402803e172cf
spmohansp <spmohansp@gmail.com>
2019-10-03 17:58:13 +0530

    swagger updated

---
+++ development/swagger-constants.php
@@ -0,0 +1,3 @@
+<?php
+// define("API_HOST", 'localhost:8000');
+define("API_HOST", 'testing.myvehicle.biz');
---
+++ development/swagger-v1.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @SWG\Swagger(
+ *     schemes={"http"},
+ *     host=API_HOST,
+ *     basePath="/",
+ *     @SWG\Info(
+ *         version="1.0.0",
+ *         title="Fleet Finance Manger",
+ *         description="Fleet Finance Manger - API for Android and IOS Development",
+ *         termsOfService="All rights reserved to Greefi Technologies",
+ *         @SWG\Contact(
+ *             email="hello@greefitech.com"
+ *         ),
+ *     ),
+ * )
+ */
