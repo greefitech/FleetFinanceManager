f118c0ab857f29dbd8dbe5d24a4c56c2613ac176
spmohansp <spmohansp@gmail.com>
2019-10-04 14:06:24 +0530

    small changes updated on index file

--- resources/views/client/layout/footer.blade.php
+++ resources/views/client/layout/footer.blade.php
@@ -2,28 +2,28 @@
     <strong>Copyright &copy; 2017- <?php echo date("Y"); ?> <a href="https://greefitech.com" target="_blank">Greefi Technologies Tiruchengode</a>.</strong> All rights reserved.
 </footer>
 </div>
-<script src="{{ url('/assets/js/app.min.js') }}"></script>
-<script src="{{ url('/assets/js/dataTables.bootstrap.min.js') }}"></script>
-<script src="{{ url('/assets/js/datatable/jquery.dataTables.min.js') }}"></script>
-<script src="{{ url('/assets/js/datatable/dataTables.responsive.min.js') }}"></script>
-<script src="{{ url('/assets/js/datatable/dataTables.bootstrap.min.js') }}"></script>
-<script src="{{ url('/assets/js/datatable/dataTables.buttons.min.js') }}"></script>
-<script src="{{ url('/assets/js/datatable/jszip.min.js') }}"></script>
-<script src="{{ url('/assets/js/datatable/pdfmake.min.js') }}"></script>
-<script src="{{ url('/assets/js/datatable/vfs_fonts.js') }}"></script>
-<script src="{{ url('/assets/js/datatable/buttons.html5.min.js') }}"></script>
-<script src="{{ url('/assets/js/datatable/buttons.print.min.js') }}"></script>
-<script src="{{ url('/assets/js/datatable/date-dd-MMM-yyyy.js') }}"></script>
-<script src="{{ url('/assets/js/datatable/jquery-ui.js') }}"></script>
-<script src="{{ url('/assets/js/validation.js') }}"></script>
+<script src="{{ asset('/assets/js/app.min.js') }}"></script>
+<script src="{{ asset('/assets/js/dataTables.bootstrap.min.js') }}"></script>
+<script src="{{ asset('/assets/js/datatable/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('/assets/js/datatable/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('/assets/js/datatable/dataTables.bootstrap.min.js') }}"></script>
+<script src="{{ asset('/assets/js/datatable/dataTables.buttons.min.js') }}"></script>
+<script src="{{ asset('/assets/js/datatable/jszip.min.js') }}"></script>
+<script src="{{ asset('/assets/js/datatable/pdfmake.min.js') }}"></script>
+<script src="{{ asset('/assets/js/datatable/vfs_fonts.js') }}"></script>
+<script src="{{ asset('/assets/js/datatable/buttons.html5.min.js') }}"></script>
+<script src="{{ asset('/assets/js/datatable/buttons.print.min.js') }}"></script>
+<script src="{{ asset('/assets/js/datatable/date-dd-MMM-yyyy.js') }}"></script>
+<script src="{{ asset('/assets/js/datatable/jquery-ui.js') }}"></script>
+<script src="{{ asset('/assets/js/validation.js') }}"></script>
 
 <script src="{{ asset('/assets/js/select2.min.js') }}"></script>
 
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js" type="text/javascript"></script>
 
-<script src="{{ url('/js/entry.js') }}"></script>
-<script src="{{ url('/js/expense.js') }}"></script>
-<script src="{{ url('/js/income.js') }}"></script>
+<script src="{{ asset('/js/entry.js') }}"></script>
+<script src="{{ asset('/js/expense.js') }}"></script>
+<script src="{{ asset('/js/income.js') }}"></script>
 
 <script>
     $(document).ready( function () {
