0aff7341b7175c9189c115cb6580ddec0426153f
VijaySBoobalan <vijayboosr@gmail.com>
2019-11-21 17:42:19 +0530

    Report Module changes

--- app/Http/Controllers/ClientController/ReportController.php
+++ app/Http/Controllers/ClientController/ReportController.php
@@ -6,6 +6,8 @@ use App\Entry;
 use App\Expense;
 use App\ExpenseType;
 use App\Vehicle;
+use App\Income;
+use App\ExtraIncome;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Excel;
@@ -24,71 +26,168 @@ class ReportController extends Controller
     }
 
     public function DownloadExpenseReport(){
-//        return request()->all();
+        // return auth()->user()->id;
+       // return request()->all();
         $this->validate(request(),[
             'vehicleId'=>'required|exists:vehicles,id',
             'expense_type'=>'required|array',
+            'report_wise'=>'required|array',
         ]);
         $Vehicle = Vehicle::findorfail(request('vehicleId'));
+        $FinalExpenseDatas = '';
+        $FinalIncomeDatas = '';
+        $FinalNonTripExpenseDatas = '';
+        $FinalExtraIncomeDatas = '';
+        if(in_array('expense',request('report_wise'))){
+            if(in_array(1,request('expense_type'))){
+                if(!empty(request('dateFrom')) && !empty(request('dateTo'))){
+                    $Entry =  Entry::select(['dateFrom AS date'])->where([['clientid',auth()->user()->id],['vehicleId',request('vehicleId')]])->whereBetween('dateFrom', [request('dateFrom'), request('dateTo')])->select(['*','dateFrom as date'])->orderBy('dateFrom')->get();
+                    $Expenses =  Expense::where([['clientid',auth()->user()->id],['vehicleId',request('vehicleId')]])->whereIn('expense_type',request('expense_type'))->whereBetween('date', [request('dateFrom'), request('dateTo')])->orderBy('date')->get();
+                }else{
+                    $Entry =  Entry::where([['clientid',auth()->user()->id],['vehicleId',request('vehicleId')]])->select(['*','dateFrom as date'])->orderBy('dateFrom')->get();
+                    $Expenses =  Expense::where([['clientid',auth()->user()->id],['vehicleId',request('vehicleId')]])->whereIn('expense_type',request('expense_type'))->orderBy('date')->get();
+                }
 
-        if(in_array(1,request('expense_type'))){
+                $FinalExpenseDatas = new \Illuminate\Database\Eloquent\Collection; //Create empty collection which we know has the merge() method
+                $FinalExpenseDatas = $FinalExpenseDatas->merge($Entry)->merge($Expenses);
+            }else{
+                if(!empty(request('dateFrom')) && !empty(request('dateTo'))){
+                    $FinalExpenseDatas =  Expense::where([['clientid',auth()->user()->id],['vehicleId',request('vehicleId')]])->whereIn('expense_type',request('expense_type'))->whereBetween('date', [request('dateFrom'), request('dateTo')])->orderBy('date')->get();
+                }else{
+                    $FinalExpenseDatas =  Expense::where([['clientid',auth()->user()->id],['vehicleId',request('vehicleId')]])->whereIn('expense_type',request('expense_type'))->orderBy('date')->get();
+                }
+
+            }
+        }
+
+        if(in_array('income',request('report_wise'))){
             if(!empty(request('dateFrom')) && !empty(request('dateTo'))){
                 $Entry =  Entry::select(['dateFrom AS date'])->where([['clientid',auth()->user()->id],['vehicleId',request('vehicleId')]])->whereBetween('dateFrom', [request('dateFrom'), request('dateTo')])->select(['*','dateFrom as date'])->orderBy('dateFrom')->get();
-                $Expenses =  Expense::where([['clientid',auth()->user()->id],['vehicleId',request('vehicleId')]])->whereIn('expense_type',request('expense_type'))->whereBetween('date', [request('dateFrom'), request('dateTo')])->orderBy('date')->get();
+                $Incomes =  Income::where([['clientid',auth()->user()->id],['vehicleId',request('vehicleId')]])->whereBetween('date', [request('dateFrom'), request('dateTo')])->orderBy('date')->get();
             }else{
                 $Entry =  Entry::where([['clientid',auth()->user()->id],['vehicleId',request('vehicleId')]])->select(['*','dateFrom as date'])->orderBy('dateFrom')->get();
-                $Expenses =  Expense::where([['clientid',auth()->user()->id],['vehicleId',request('vehicleId')]])->whereIn('expense_type',request('expense_type'))->orderBy('date')->get();
+                $Incomes =  Income::where([['clientid',auth()->user()->id],['vehicleId',request('vehicleId')]])->orderBy('date')->get();
             }
 
-            $FinalSelectDatas = new \Illuminate\Database\Eloquent\Collection; //Create empty collection which we know has the merge() method
-            $FinalSelectDatas = $FinalSelectDatas->merge($Entry);
-            $FinalSelectDatas = $FinalSelectDatas->merge($Expenses);
-
-            $FinalSelectDatas->sortBy('date');
-
-        }else{
-            if(!empty(request('dateFrom')) && !empty(request('dateTo'))){
-                $FinalSelectDatas =  Expense::where([['clientid',auth()->user()->id],['vehicleId',request('vehicleId')]])->whereIn('expense_type',request('expense_type'))->whereBetween('date', [request('dateFrom'), request('dateTo')])->orderBy('date')->get();
-            }else{
-                $FinalSelectDatas =  Expense::where([['clientid',auth()->user()->id],['vehicleId',request('vehicleId')]])->whereIn('expense_type',request('expense_type'))->orderBy('date')->get();
-            }
-
+            $FinalIncomeDatas = new \Illuminate\Database\Eloquent\Collection; //Create empty collection which we know has the merge() method
+            $FinalIncomeDatas = $FinalIncomeDatas->merge($Entry)->merge($Incomes);
         }
 
+        if(in_array('non_trip_expense',request('report_wise'))){
+            if(in_array(1,request('expense_type'))){
+                if(!empty(request('dateFrom')) && !empty(request('dateTo'))){
+                    $Entry =  Entry::select(['dateFrom AS date'])->where([['clientid',auth()->user()->id],['vehicleId',request('vehicleId')]])->whereBetween('dateFrom', [request('dateFrom'), request('dateTo')])->select(['*','dateFrom as date'])->orderBy('dateFrom')->get();
+                    $Expenses =  Expense::where([['clientid',auth()->user()->id],['vehicleId',request('vehicleId')],['tripId','=',NULL]])->whereIn('expense_type',request('expense_type'))->whereBetween('date', [request('dateFrom'), request('dateTo')])->orderBy('date')->get();
+                }else{
+                    $Entry =  Entry::where([['clientid',auth()->user()->id],['vehicleId',request('vehicleId')]])->select(['*','dateFrom as date'])->orderBy('dateFrom')->get();
+                    $Expenses =  Expense::where([['clientid',auth()->user()->id],['vehicleId',request('vehicleId')],['tripId','=',NULL]])->whereIn('expense_type',request('expense_type'))->orderBy('date')->get();
+                }
+
+                $FinalNonTripExpenseDatas = new \Illuminate\Database\Eloquent\Collection; //Create empty collection which we know has the merge() method
+                $FinalNonTripExpenseDatas = $FinalNonTripExpenseDatas->merge($Entry)->merge($Expenses);
+            }else{
+                if(!empty(request('dateFrom')) && !empty(request('dateTo'))){
+                    $FinalNonTripExpenseDatas =  Expense::where([['clientid',auth()->user()->id],['vehicleId',request('vehicleId')]])->whereIn('expense_type',request('expense_type'))->whereBetween('date', [request('dateFrom'), request('dateTo')])->orderBy('date')->get();
+                }else{
+                    $FinalNonTripExpenseDatas =  Expense::where([['clientid',auth()->user()->id],['vehicleId',request('vehicleId')]])->whereIn('expense_type',request('expense_type'))->orderBy('date')->get();
+                }
+
+            }
+        }
+
+        if(in_array('extra_income',request('report_wise'))){
+            if(in_array(1,request('expense_type'))){
+                if(!empty(request('dateFrom')) && !empty(request('dateTo'))){
+                    $Entry =  Entry::select(['dateFrom AS date'])->where([['clientid',auth()->user()->id],['vehicleId',request('vehicleId')]])->whereBetween('dateFrom', [request('dateFrom'), request('dateTo')])->select(['*','dateFrom as date'])->orderBy('dateFrom')->get();
+                    $ExtraIncomes =  ExtraIncome::where([['clientid',auth()->user()->id],['vehicleId',request('vehicleId')]])->whereIn('expense_type',request('expense_type'))->whereBetween('date', [request('dateFrom'), request('dateTo')])->orderBy('date')->get();
+                }else{
+                    $Entry =  Entry::where([['clientid',auth()->user()->id],['vehicleId',request('vehicleId')]])->select(['*','dateFrom as date'])->orderBy('dateFrom')->get();
+                    $ExtraIncomes =  ExtraIncome::where([['clientid',auth()->user()->id],['vehicleId',request('vehicleId')]])->whereIn('expense_type',request('expense_type'))->orderBy('date')->get();
+                }
+
+                $FinalExtraIncomeDatas = new \Illuminate\Database\Eloquent\Collection; //Create empty collection which we know has the merge() method
+                $FinalExtraIncomeDatas = $FinalExtraIncomeDatas->merge($Entry)->merge($ExtraIncomes);
+            }else{
+                if(!empty(request('dateFrom')) && !empty(request('dateTo'))){
+                    $FinalExtraIncomeDatas =  ExtraIncome::where([['clientid',auth()->user()->id],['vehicleId',request('vehicleId')]])->whereIn('expense_type',request('expense_type'))->whereBetween('date', [request('dateFrom'), request('dateTo')])->orderBy('date')->get();
+                }else{
+                    $FinalExtraIncomeDatas =  ExtraIncome::where([['clientid',auth()->user()->id],['vehicleId',request('vehicleId')]])->whereIn('expense_type',request('expense_type'))->orderBy('date')->get();
+                }
+
+            }
+        }
+       
+        $merged = collect($FinalExpenseDatas)->merge($FinalIncomeDatas)->merge($FinalNonTripExpenseDatas)->merge($FinalExtraIncomeDatas);
+        return $FinalSelectDatas = $merged->all();
         foreach ($FinalSelectDatas as $key => $value) {
-            if(!empty($value->ExpenseType->expenseType)){
-                $ExpenseType = @$value->ExpenseType->expenseType;
-            }else{
-                $ExpenseType = 'டிரைவர் படி';
+            if(in_array('expense',request('report_wise'))){
+                if(!empty($value->ExpenseType->expenseType)){
+                    $ExpenseType = @$value->ExpenseType->expenseType;
+                }else{
+                    $ExpenseType = 'டிரைவர் படி';
+                }
+                $StaffName='';
+                if(!empty($value->dateFrom)){
+                    $StaffName = $value->Trip->Staff1->name;
+                }
+                if(!empty($value->amount)){
+                    $Amount = @$value->amount;
+                }else{
+                    $Amount = @$value->driverPadiAmount + @$value->cleanerPadiAmount;
+                }
+                $finalData[] =array(
+                    'Date' => @$value->date,
+                    'Expense Type' => $ExpenseType,
+                    'Staff Name' => @$StaffName,
+                    'Quantity' => @$value->quantity,
+                    'Amount' => $Amount,
+                    'Location' => @$value->location,
+                );
             }
-            $StaffName='';
-            if(!empty($value->dateFrom)){
-                $StaffName = $value->Trip->Staff1->name;
+            if(in_array('income',request('report_wise'))){
+                if(!empty($value->Trip->tripName)){
+                    $TripName = @$value->Trip->tripName;
+                }else{
+                    $TripName = '';
+                }
+                $StaffName='';
+                if(!empty($value->dateFrom)){
+                    $StaffName = $value->Trip->Staff1->name;
+                }
+                if(!empty($value->recevingAmount)){
+                    $Amount = $value->recevingAmount;
+                }
+                $finalData[] =array(
+                    'Date' => @$value->date,
+                    'Expense Type' => $TripName,
+                    'Staff Name' => @$StaffName,
+                    'Quantity' => @$value->quantity,
+                    'Amount' => @$Amount,
+                    'Location' => @$value->location,
+                );
             }
-            if(!empty($value->amount)){
-                $Amount = @$value->amount;
-            }else{
-                $Amount = $value->driverPadiAmount + $value->cleanerPadiAmount;
+
+            if(in_array('non_trip_expense',request('report_wise'))){
+                
             }
+
+            if(in_array('extra_income',request('report_wise'))){
+                
+            }
+        }
+
+        return 1;
+        $finalData[] =array();
+        if(in_array('income',request('report_wise'))){
             $finalData[] =array(
-                'Date' => $value->date,
-                'Expense Type' => $ExpenseType,
-                'Staff Name' => @$StaffName,
-                'Quantity' => $value->quantity,
-                'Amount' => $Amount,
-                'Location' => $value->location,
+                'Date' => '',
+                'Expense Type' => 'Total ',
+                'Staff Name' => '',
+                'Quantity' => '',
+                'Amount' => @$FinalSelectDatas->sum('recevingAmount'),
+                'Location' => '',
             );
         }
-
-        $finalData[] =array();
-        $finalData[] =array(
-            'Date' => '',
-            'Expense Type' => 'Total ',
-            'Staff Name' => '',
-            'Quantity' => '',
-            'Amount' => $FinalSelectDatas->sum('amount') + $FinalSelectDatas->sum('driverPadiAmount') + $FinalSelectDatas->sum('cleanerPadiAmount'),
-            'Location' => '',
-        );
 
         if(!empty($finalData)){
             Excel::create($Vehicle->vehicleNumber.' - Report - '.date('Y-m-d'),function($excel) use ($finalData,$Vehicle){
--- resources/views/client/report/expense-report.blade.php
+++ resources/views/client/report/expense-report.blade.php
@@ -15,7 +15,7 @@
                         {{ csrf_field() }}
                         <div class="box-body">
                             <div class="row">
-                                <div class="col-sm-3">
+                                <div class="col-sm-4">
                                     <div class="form-group{{ $errors->has('dateFrom') ? ' has-error' : '' }}">
                                         <div class="col-sm-12">
                                             <label>Date From</label>
@@ -23,7 +23,7 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-sm-3">
+                                <div class="col-sm-4">
                                     <div class="form-group{{ $errors->has('dateTo') ? ' has-error' : '' }}">
                                         <div class="col-sm-12">
                                             <label>Date To</label>
@@ -31,7 +31,7 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-sm-3">
+                                <div class="col-sm-4">
                                     <div class="form-group{{ $errors->has('vehicleId') ? ' has-error' : '' }}">
                                         <div class="col-sm-12">
                                             <label>Vehicle</label>
@@ -44,15 +44,36 @@
                                         </div>
                                     </div>
                                 </div>
+                            </div>
+                            <div class="row">
                                 <div class="col-sm-3">
                                     <div class="form-group{{ $errors->has('expense_type') ? ' has-error' : '' }}">
                                         <div class="col-sm-12">
                                             <label>Expense</label>
                                             <select name="expense_type[]" class="form-control" multiple id="entry-type">
                                                 @foreach($ExpenseTypes as $ExpenseType)
-                                                    <option value="{{ $ExpenseType->id }}" {{ ($ExpenseType->id == old('expense_type'))?'selected':'' }}>{{ $ExpenseType->expenseType }}</option>
+                                                    <option value="{{ $ExpenseType->id }}" {{ !empty(old('expense_type'))? in_array($ExpenseType->id,old('expense_type')) ? 'selected' : '' : '' }}>{{ $ExpenseType->expenseType }}</option>
                                                 @endforeach
                                             </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-9">
+                                    <div class="form-group{{ $errors->has('report_wise') ? ' has-error' : '' }}">
+                                    <label>Report Wise</label>
+                                        <div class="form-check-inline">
+                                            <label class="form-check-label">
+                                                <input type="checkbox" name="report_wise[]" class="form-check-input" value="expense" {{ !empty(old('report_wise'))? in_array('expense',old('report_wise')) ? 'checked' : '' : '' }}>&nbsp;&nbsp;Expense
+                                            </label>&nbsp;&nbsp;&nbsp;&nbsp;
+                                            <label class="form-check-label">
+                                                <input type="checkbox" name="report_wise[]" class="form-check-input" value="income" {{ !empty(old('report_wise'))?in_array('income',old('report_wise')) ? 'checked' : '' : '' }}>&nbsp;&nbsp;Income
+                                            </label>&nbsp;&nbsp;&nbsp;&nbsp;
+                                            <label class="form-check-label">
+                                                <input type="checkbox" name="report_wise[]" class="form-check-input" value="non_trip_expense" {{ !empty(old('report_wise'))?in_array('non_trip_expense',old('report_wise')) ? 'checked' : '' : '' }}>&nbsp;&nbsp;Non-Trip Expense
+                                            </label>&nbsp;&nbsp;&nbsp;&nbsp;
+                                            <label class="form-check-label">
+                                                <input type="checkbox" name="report_wise[]" class="form-check-input" value="extra_income" {{ !empty(old('report_wise'))?in_array('extra_income',old('report_wise')) ? 'checked' : '' : '' }}>&nbsp;&nbsp;Extra Income
+                                            </label>&nbsp;&nbsp;&nbsp;&nbsp;
                                         </div>
                                     </div>
                                 </div>
