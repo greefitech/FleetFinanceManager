37f647bdb085e71edcebf61ff4521fd1f7ddb9f4
spmohansp <spmohansp@gmail.com>
2020-03-09 15:08:15 +0530

    small changes updated

--- config/snappy.php
+++ config/snappy.php
@@ -1,8 +1,6 @@
 <?php
 
 return array(
-
-
     'pdf' => array(
         'enabled' => true,
         'binary'  => '/usr/local/bin/wkhtmltopdf-amd64',
@@ -17,6 +15,22 @@ return array(
         'options' => array(),
         'env'     => array(),
     ),
-
-
 );
+
+
+// return array(
+//      'pdf' => array(
+//         'enabled' => true,
+//         'binary'  => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
+//         'timeout' => false,
+//         'options' => array(),
+//         'env'     => array(),
+//     ),
+//     'image' => array(
+//         'enabled' => true,
+//         'binary'  => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltoimage-amd64'),
+//         'timeout' => false,
+//         'options' => array(),
+//         'env'     => array(),
+//     ),
+// );
