dff95ffd637fe8008add08839164f28d581cbd8f
spmohansp <spmohansp@gmail.com>
2020-03-17 14:46:16 +0530

    client document mail updated and document queue changes updated

--- app/Http/Controllers/BasicController.php
+++ app/Http/Controllers/BasicController.php
@@ -8,17 +8,26 @@ use App\Vehicle;
 
 use App\Mail\SendDocumentNotification;
 use App\Jobs\SendClientMonthlyIncomeExpense;
+use App\Jobs\SendClientDocumentNotification;
 use Mail;
 
 class BasicController extends Controller
 {
+
+    /*
+    *----------------------------------------
+    * Send mail for client profit expense
+    *----------------------------------------
+    *This function is used to send the mail for all clients monthly profit and expense list
+    */
     public function ClientMonthlyIncomeExpenseMail() {
+        /*Dont remove this code its for development checking for single client by mohan*/
         if(env('APP_ENV') =='localmohan'){
     		$client = Client::findorfail(3);
     		SendClientMonthlyIncomeExpense::dispatch($client)->delay(now()->addSecond(10));
         }
 
-
+        /*It's production code for sending client email*/
         if(env('APP_ENV') =='production'){
     		$clients = Client::where('mail_notification',1)->get();
     		foreach ($clients as $key => $client) {
@@ -28,17 +37,23 @@ class BasicController extends Controller
 		dd('done');
     }
 
-
+    /*
+    *---------------------------------------------
+    * Send mail for document notification
+    *---------------------------------------------
+    *This function is used to send the document renewal notification to all clents
+    */
     public function ClientDocumentExpireMail() {
-
+        /*Dont remove this code its for development checking for single client by mohan*/
         if(env('APP_ENV') =='localmohan'){
             $data['client'] = Client::findorfail(3);
             $data['clientVehicles'] = Vehicle::where([['clientid',$data['client']->id],['vehicle_status',1],['document_types.mail_notification',1]])->join('documents','documents.vehicleId','vehicles.id')->join('document_types','document_types.id','documents.documentType')->get();
-            $email = new SendDocumentNotification($data);
-            Mail::to($data['client']->email)->send($email);
+            SendClientDocumentNotification::dispatch( $data)->delay(now()->addSecond(10)); // Send mail 
+            // $email = new SendDocumentNotification($data);
+            // Mail::to($data['client']->email)->send($email);
         }
 
-
+        /*Dont remove this code by mohan*/
     	// foreach ($clientVehicles as $clientVehicleKey => $clientVehicle) {
     	// 	// return $clientVehicle;
     	// 	// return  DateDifference($clientVehicle->duedate);
@@ -48,14 +63,15 @@ class BasicController extends Controller
     	// 	}
     	// }
            
-
+        /*It's production code for sending client email*/
         if(env('APP_ENV') =='production'){
             $clients =  Client::where('mail_notification',1)->get();
             foreach ($clients as $key => $client) {
                 $data['client'] = $client;
                 $data['clientVehicles'] = Vehicle::where([['clientid',$client->id],['vehicle_status',1],['document_types.mail_notification',1]])->join('documents','documents.vehicleId','vehicles.id')->join('document_types','document_types.id','documents.documentType')->get();
-                $email = new SendDocumentNotification($data);
-                Mail::to($client->email)->send($email);
+                SendClientDocumentNotification::dispatch( $data)->delay(now()->addSecond(10));
+                // $email = new SendDocumentNotification($data);
+                // Mail::to($client->email)->send($email);
             }
         }
         dd('done');
